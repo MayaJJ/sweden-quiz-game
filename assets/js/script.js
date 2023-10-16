8ced5a8d3d1a7470e2183b5a2b1fd82c04e84d39
MayaJJ <127303012+MayaJJ@users.noreply.github.com>
2023-10-17 01:54:00 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,7 +95,7 @@ const quizArray = [
       "No peets allowed indoors",
       "No eating infront of the TV",
     ],
-    correct: "Take your shoes off indoors",
+    correct: "Take your shoes of indoors",
   },
   {
     id: "9",
